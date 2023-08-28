c979ac8263072aa4602e892440fdd6590b181e5a
SleepWalker <dev@udf.su>
2023-08-28 16:27:37 +0300

    chore: add support for more prefixes for changelog generation

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,30 @@
 const preset = {
   preset: 'conventionalcommits',
+  releaseRules: [
+    // support adding "!" suffix to trigger breaking (major) release
+    { breaking: true, release: 'major' },
+    { type: 'feature', release: 'minor' },
+    { type: 'ref', release: 'patch' },
+    // the other rules (e.g. feat, fix, perf) are defined here:
+    // https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
+  ],
+  presetConfig: {
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'feature', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'perf', section: 'Performance Improvements' },
+      { type: 'revert', section: 'Reverts' },
+      { type: 'refactor', section: 'Code Refactoring' },
+      { type: 'ref', section: 'Code Refactoring' },
+      { type: 'docs', section: 'Documentation', hidden: true },
+      { type: 'style', section: 'Styles', hidden: true },
+      { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+      { type: 'test', section: 'Tests', hidden: true },
+      { type: 'build', section: 'Build System', hidden: true },
+      { type: 'ci', section: 'Continuous Integration', hidden: true },
+    ],
+  },
 };
 
 module.exports = {
