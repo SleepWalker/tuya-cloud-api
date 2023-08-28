2a46735000fb439c97366c0866279ba39810ef8b
SleepWalker <dev@udf.su>
2023-08-28 16:41:35 +0300

    chore: commit package-lock during release

--- .releaserc.js
+++ .releaserc.js
@@ -60,7 +60,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json'],
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
       },
     ],
   ],
